6f7adfcb9ab503c00e01eae8100f0c13033b1ad3
Finn <50817575+Finnventor@users.noreply.github.com>
2024-01-09 23:30:23 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("./README.rst", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="plopy",
+    version="1.6.8",
+    author="Finnventor",
+    description="GUI for matplotlib graphing",
+    long_description=long_description,
+    url="https://github.com/Finnventor/plopy",
+    packages=setuptools.find_packages(),
+    package_data={'plopy': ['*.txt'], 'plopy.ui': ['*.png']},
+    install_requires=['python-dateutil>=2.8.1', 'PySide6', 'matplotlib'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent"
+    ]
+)
