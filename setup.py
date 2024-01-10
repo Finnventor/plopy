b9389c2eb4d1d17059d9cfd69133832a71590a06
Finn <50817575+Finnventor@users.noreply.github.com>
2024-01-09 23:58:29 -0800

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("./README.rst", "r") as fh:
 
 setuptools.setup(
     name="plopy",
-    version="1.6.8",
+    version="1.6.9",
     author="Finnventor",
     description="GUI for matplotlib graphing",
     long_description=long_description,
